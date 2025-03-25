d922744ed693b62465928793385cf65ef03d0dce
LokeshMunusamy <lokeshmdckap@gmail.com>
2025-03-25 21:46:05 +0530

    add file

--- script.js
+++ script.js
@@ -1,67 +1,189 @@
-// cuisines
-// https://api.spoonacular.com/recipes/complexSearch?cuisine=indian&apiKey=61ba7261179a496cb67d893408817c48;
+// let apiKey = '68a32d20b82e4216bbc04947c8af0512';
 
-// search particular resipe
+// let apiUrl = 'https://api.spoonacular.com/recipes/random'; 
 
-// https://api.spoonacular.com/recipes/complexSearch?query=Rice and Peas with Coconut Curry Mackerel&apiKey=61ba7261179a496cb67d893408817c48
+// let cuisinesLink = 'https://api.spoonacular.com/recipes/complexSearch';
 
-// https://api.spoonacular.com/recipes/random?apiKey=61ba7261179a496cb67d893408817c48
+// let addNumber = 0; 
+
+// let loader = document.querySelector('.loader-container');
+
+// async function recipeData() {
+    
+//     let query = `${apiUrl}?apiKey=${apiKey}&number=${12+addNumber}`;
+    
+//     try {
+//         const response = await fetch(query);
+//         loader.style.display = 'block';
+//         if (!response.ok) {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         displayProduct(data.recipes);
+//         scrollImg(data.recipes); 
+        
+//     } catch (error) {
+//         console.error(error); 
+//     }
+// }
+
+// recipeData(); 
+
+// function scrollImg(apiData) {
+//     let scroll = document.querySelector('.scroll-recipe');
+//     scroll.innerHTML = ''; 
+
+//     apiData.forEach(ele => {
+//         scroll.innerHTML += `
+//             <div class="recipe-scroll"> 
+//                 <img src="${ele.image}" alt="Recipe Image">
+//                 <p>${ele.title}</p>
+//             </div>`;
+//     });
+//     loader.style.display = 'none';
+// }
+
+// let flag = true;
+
+// function displayProduct(apiData) {
+//     let recipesContainer = document.querySelector('.recipe-container');
+//     let filterBtn = document.querySelector('.filterbtn');
+//     let cuisine=['All']
+//     if(!flag)recipesContainer.innerHTML = '';
+    
+//     apiData.forEach(ele => {
+//         const recipeDiv = document.createElement('div');
+//         recipeDiv.classList.add('recipe-data');
+//         recipeDiv.innerHTML = `
+//             <img src="${ele.image}" alt="Recipe Image">
+//             <p>${ele.title}</p>
+//         `;
+
+//         recipesContainer.appendChild(recipeDiv);
+        
+//         cuisine = [...cuisine,...ele.cuisines]
+//          cuisine=[...new Set(cuisine)];
+
+//          recipeDiv.addEventListener('click',()=>{
+//             document.querySelector('.popup').innerHTML = ` <h1>${ele.title}</h1>
+//                                                         <img src=${ele.image} alt="Product image">
+//                                                         <p>${ele.pricePerServing}</p>
+//                                                          <i class="fas fa-times popup-xmark"></i> `;
+            
+//             document.querySelector('.popup').style.display = 'block';
+//          })
+
+//         //  console.log(cuisine);
+
+//     });
+//     recipeDiv.querySelector('.popup-xmark').addEventListener('click',()=>{
+//         document.querySelector('.popup').style.display = 'none';
+//     })
+
+//     loader.style.display = 'none';
+
+//     filterBtn.innerHTML = cuisine.map(x=>`<button class ='filtercountry'>${x}</button>`).join(''); 
+
+//     let filterCuisine = filterBtn.querySelectorAll('.filtercountry');
+
+    
+//         filterCuisine.forEach((btn)=>{
+//             btn.addEventListener('click',()=>{
+        
+//             console.log(btn.textContent);
+//             searchAndCuisine(country = `${btn.textContent}`) 
+                
+//         })
+//     })
+    
+// }
+
+// async function searchAndCuisine(search,country){
+    
+//     let query = `${cuisinesLink}?apiKey=${apiKey}&${search ?`query=${search}`:`type=${country}`}`;
+
+//     loader.style.display = 'block';
+
+//     try {
+//         const response = await fetch(query);
+//         if (!response.ok) {
+//             throw new Error(`Response status: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         if(search){
+//             scrollImg(data.results);
+//         }else{
+//             displayProduct(data.results);
+//             flag = false;
+//         }
+        
+        
+//     } catch (error) {
+//         console.error(error); 
+//     }
+    
+// }
+
+// document.querySelector('.search-btn').addEventListener('click',()=>{
+//     let inputValue = document.querySelector('.search').value.trim();
+//     searchAndCuisine(search=`${inputValue}`);
+// })
+
+// document.querySelector('.add-more').addEventListener('click',()=>{
+//     addNumber+=12;
+//     recipeData()
+// })
 
 
-// nested ternary
-
-// let age = 25;
-
-// let category = age < 18 ? 'Child' :
-//                (age < 21 ? 'Young Adult' : 
-//                (age < 60 ? 'Adult' : 'Senior'));
-
-// console.log(category);
-
-
-
-let apiKey = '059bae3dc36242be90a54696706936f8';
+let apiKey = '68a32d20b82e4216bbc04947c8af0512';
 let apiUrl = 'https://api.spoonacular.com/recipes/random'; 
 let cuisinesLink = 'https://api.spoonacular.com/recipes/complexSearch';
+let addNumber = 0; 
+let loader = document.querySelector('.loader-container');
 
-let cuisine;
+async function recipeData() {
+    let query = `${apiUrl}?apiKey=${apiKey}&number=${12 + addNumber}`;
 
-async function recipeData(cuisine) {
-    let query = cuisine ? `${cuisinesLink}?cuisine=${cuisine}&apiKey=${apiKey}` : `${apiUrl}?apiKey=${apiKey}&number=15`;
     try {
+        loader.style.display = 'block'; 
         const response = await fetch(query);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
-        const data = await response.json(); 
-
-        displayProduct(data.recipes); 
-        scrollImg(data.recipes); 
-        
+        const data = await response.json();
+        displayProduct(data.recipes);
+        scrollImg(data.recipes);
     } catch (error) {
-        console.error(error); 
+        console.error(error);
+        loader.style.display = 'none'; 
     }
 }
 
-recipeData(); 
+recipeData();
 
-function scrollImg(apiData,){
+function scrollImg(apiData) {
     let scroll = document.querySelector('.scroll-recipe');
-    scroll.innerHTML = ''; 
+    scroll.innerHTML = '';
 
     apiData.forEach(ele => {
         scroll.innerHTML += `
-            <div class="recipe-scroll"> 
+            <div class="recipe-scroll">
                 <img src="${ele.image}" alt="Recipe Image">
                 <p>${ele.title}</p>
             </div>`;
-
     });
 
+    loader.style.display = 'none'; 
 }
+
+let flag = true;
 
 function displayProduct(apiData) {
     let recipesContainer = document.querySelector('.recipe-container');
+    let filterBtn = document.querySelector('.filterbtn');
+    let cuisine = ['All'];
+
+    if (!flag) recipesContainer.innerHTML = '';
 
     apiData.forEach(ele => {
         const recipeDiv = document.createElement('div');
@@ -71,127 +193,74 @@ function displayProduct(apiData) {
             <p>${ele.title}</p>
         `;
 
-
-        recipeDiv.addEventListener('click', () => {
-            console.log(ele.title); 
-        });
-
         recipesContainer.appendChild(recipeDiv);
 
+        
+        cuisine = [...cuisine, ...ele.cuisines];
+        cuisine = [...new Set(cuisine)];
+
+        recipeDiv.addEventListener('click', () => {
+            const popup = document.querySelector('.popup');
+            popup.innerHTML = `
+                <h1>${ele.title}</h1>
+                <img src="${ele.image}" alt="Product image">
+                <p>${ele.pricePerServing}</p>
+                <i class="fas fa-times popup-xmark"></i>
+            `;
+            popup.style.display = 'block';
+
+            
+            const closeBtn = popup.querySelector('.popup-xmark');
+            closeBtn.addEventListener('click', () => {
+                popup.style.display = 'none';
+            });
+        });
     });
-};
 
-document.querySelector('.add-more').addEventListener('click',()=>{
-    recipeData();
-})
+    
+    filterBtn.innerHTML = cuisine.map(x => `<button class='filtercountry'>${x}</button>`).join('');
 
-async function searchAndCuisines(recipeName,counry){
+    let filterCuisine = filterBtn.querySelectorAll('.filtercountry');
+    filterCuisine.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            console.log(btn.textContent);
+            searchAndCuisine(country = `${btn.textContent}`);
+        });
+    });
 
-    let query = `${cuisinesLink}?query=${recipeName}&apiKey=${apiKey}`; 
+    loader.style.display = 'none'; 
+}
+
+async function searchAndCuisine(search, country) {
+    let query = `${cuisinesLink}?apiKey=${apiKey}&${search ? `query=${search}` : `type=${country}`}`;
+
+    loader.style.display = 'block'; 
+
     try {
         const response = await fetch(query);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
-        const data = await response.json(); 
-
-       
-        scrollImg(data.recipes); 
-        
-        
+        const data = await response.json();
+        if (search) {
+            scrollImg(data.results);
+        } else {
+            displayProduct(data.results);
+            flag = false;
+        }
     } catch (error) {
-        console.error(error); 
+        console.error(error);
+        loader.style.display = 'none'; 
     }
 }
 
-let searchBtn = document.querySelector('.search-btn'); 
-
-searchBtn.addEventListener('click',()=>{
-    let searchRecipe = document.querySelector('.search').value.trim();
-    searchAndCuisines(recipeName = `${searchRecipe}`);
-
-})
-
-
-let country = document.querySelectorAll('li');
-
-country.forEach(ele => {
-    ele.addEventListener('click', () => {
-
-        if(ele.textContent == 'All'){
-            recipeData();
-        }else{
-            recipeData(cuisine=`${ele.textContent.toLowerCase()}`)
-        }
-        console.log(ele.textContent); 
-    });
+document.querySelector('.search-btn').addEventListener('click', () => {
+    let inputValue = document.querySelector('.search').value.trim();
+    searchAndCuisine(search = `${inputValue}`);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let cuisines;
-
-// let apiKey = '61ba7261179a496cb67d893408817c48'; 
-// let apiUrl = 'https://api.spoonacular.com/recipes/random'; 
-// let cuisinesLink = 'https://api.spoonacular.com/recipes/complexSearch';
-
-// async function recipeData(search, cuisines) {
-    
-//     let query = search ? `${cuisinesLink}?query=${search}&apiKey=${apiKey}&number=15`
-//                 : cuisines ? `${cuisinesLink}?cuisine=${cuisines}&apiKey=${apiKey}&number=15`
-//                 : `${apiUrl}?apiKey=${apiKey}&number=15`;  
-
-//     try {
-//         const response = await fetch(query);
-//         if (!response.ok) {
-//             throw new Error(`Response status: ${response.status}`);
-//         }
-//         const data = await response.json();
-//         cuisines = response.cuisines
-//         console.log(cuisines);
-        
-        
-//         const recipes = data.results || data.recipes || [];
-
-        
-//         if (recipes.length > 0) {
-//             displayProduct(recipes);
-//         } else {
-//             console.log("No recipes found.");
-//         }
-
-//     } catch (error) {
-//         console.error('Error:', error); 
-//     }
-// }
-
-
-// recipeData(); 
-
-// function displayProduct(apiData) {
-//     let recipesContainer = document.querySelector('.recipe-container');
-//     // recipesContainer.innerHTML = '';      
-//     apiData.forEach(ele => {
-//         recipesContainer.innerHTML += `
-//             <div class="recipe-data">
-//                 <img src="${ele.image}" alt="Recipe Image">
-//                 <p class='api-title'>${ele.title}</p>
-//             </div>`;
-//     });
-// }
-
-
-
-
+document.querySelector('.add-more').addEventListener('click', () => {
+    addNumber += 12;
+    recipeData();
+});
 
